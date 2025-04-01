bc718933becc3a3c17fb4994bddc1dc8ac63986c
logicfool <logicfool123121@gmail.com>
2025-04-02 00:09:04 +0530

    Converter Fix UI and Sitemap

--- src/utils/authConfig.ts
+++ src/utils/authConfig.ts
@@ -1,5 +1,5 @@
 // This file contains only the client-safe configuration options
 export const authConfig = {
-  basePath: "/price/api/auth",
+  basePath: "/api/auth",
   baseUrl: typeof window !== 'undefined' ? window.location.origin : '',
 };
